b8380ca5b6eb2762d5168a9da03c95bb69cbf890
gursoy <gursoy@DESKTOP-JP87747>
2021-01-21 22:23:13 +0300

    Update(21.01.2021)

---
+++ Math/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Math
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BasicOperations dortIslem = new BasicOperations();
+            dortIslem.Topla(5, 6);
+            dortIslem.Topla(6, 9);
+        }
+    }
+}
---
+++ Methods/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace Methods
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string urunAdi = "Elma";
+            double fiyati = 15;
+            string aciklama = "Amasya elması";
+
+            string[] meyveler = new string[] { "Elma", "Karpuz" };
+
+            Product urun1 = new Product();
+            urun1.Adi = "Elma";
+            urun1.Fiyati = 15;
+            urun1.Aciklama = "Amasya elması";
+
+            Product urun2 = new Product();
+            urun2.Adi = "Karpuz";
+            urun2.Fiyati = 80;
+            urun2.Aciklama = "Diyarbakır karpuzu";
+
+            Product[] urunler = new Product[] { urun1, urun2 };
+
+            //type-safe -- tip güvenli
+            foreach (var urun in urunler)
+            {
+                Console.WriteLine(urun.Adi);
+                Console.WriteLine(urun.Fiyati);
+                Console.WriteLine(urun.Aciklama);
+                Console.WriteLine("--------------------");
+            }
+
+            Console.WriteLine("------------Metotlar---------------");
+            //instance - örnek
+            //encapsulation
+
+            BasketManager sepetManager = new BasketManager();
+            sepetManager.Ekle(urun1);
+            sepetManager.Ekle(urun2);
+
+            sepetManager.Ekle2("Armut", "Yeşil armut", 12, 10);
+            sepetManager.Ekle2("Elma", "Yeşil elma", 12, 9);
+            sepetManager.Ekle2("Karpuz", "Diyarbakır karpuzu", 12, 8);
+        }
+    }
+}
